cd22041b479eecb388bff4fb62bff4fbf62469aa
rayan <rayanfayadhussein98@gmail.com>
2022-12-13 20:37:00 +0200

    multiples

---
+++ problem4.js
@@ -0,0 +1,12 @@
+function show(a){
+    var a=document.getElementById("num").value;
+    var cal=0;
+    for (i=1;i<=a;i++) {
+      if(i%3==0){
+       cal+=i;
+      }else if(i%5==0){
+         cal+=i;
+      }}
+
+      alert(cal) 
+}

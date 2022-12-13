ad143b314cc42d8e4e77394e57afe6275071cf2a
rayan <rayanfayadhussein98@gmail.com>
2022-12-13 20:31:35 +0200

    condition

---
+++ problem2.js
@@ -0,0 +1,10 @@
+function show(){
+    var a=document.getElementById("Name").value;
+    if ((a=="Bob") || (a=="Alice")) {
+    alert("hello "+a)
+    }
+    
+    else{
+    alert("hello Stranger") 
+    }
+    }
--- problem3.js
+++
@@ -1,8 +0,0 @@
-function show(){
-    var a=document.getElementById("Number").value;
-    var s=0;
-    for (var i= 1; i<=a; a++) {
-s+=i;
-alert(s);
-    }
-}
